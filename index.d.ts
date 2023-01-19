a48eb42bd2274147ad4112aa3370ddefe0bec2b5
Sean Freeman <theuimaster@gmail.com>
2023-01-19 14:26:11 +0530

    added types

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export default class ActionStore {
+    subscriptions: never[];
+    clearTimeOut: null;
+    version: number;
+    siblings: never[];
+}
+export declare let subScribe: (obj: any, func: any, label: any) => void;
+export declare let update: (obj: any, throttle?: number, updateSiblings?: boolean) => void;
+export declare let updateAll: () => void;
+export declare let unSubscribe: (obj: any, label: any) => void;
+export declare let stateChanged: (state: any, throttle: number, updateSiblings: any) => void;
+export declare const getRandom: (length: number) => string;
+export declare const useSubscribe: (...props: any) => void;
