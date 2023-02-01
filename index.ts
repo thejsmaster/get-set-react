5c32487b3152fb09216f84a3c29ad86c2b67c8fc
Sean Freeman <sean.freeman@pixis.ai>
2023-02-01 20:02:34 +0530

    improved functionality. added support for memo and onset

--- index.ts
+++ index.ts
@@ -1,84 +1,112 @@
 import { useEffect, useState } from "react";
-
 export class ActionStore {
   subscriptions = [];
   clearTimeOut = null;
   version = 0;
   siblings = [];
+  onSetInProgress = false;
+  setList = [];
+}
+const catchError = (e: any = "", key = "") => {
+  console.error("error occured in this function '" + key + " '. ", e);
+  throw new Error(
+    "error occured in '" + key + "' function : check console: " + e
+  );
+  return;
+};
+let stateUpdateContext: any = null;
+const snapchats: any = {};
+export function copy(state: any) {
+  return state && JSON.parse(JSON.stringify(state));
+}
+export function saveCopy(state: any, label: string) {
+  state && label && (snapchats[label] = state);
+}
+export function getSavedCopy(label: string) {
+  return snapchats[label];
 }
 
 function getSetJS<T>(obj: T | any) {
+  obj.__ = new ActionStore();
   if (
     obj !== null &&
     obj !== undefined &&
     typeof obj === "object" &&
-    !Array.isArray(obj)
+    !Array.isArray(obj) &&
+    obj.set !== null &&
+    obj.set !== undefined &&
+    typeof obj.set === "object" &&
+    !Array.isArray(obj.set)
   ) {
     let keys = [
-      ...Object.keys(obj.__proto__),
-      ...Object.keys(obj as object),
-    ].filter((key) => key.startsWith("set"));
-    console.log(keys);
+      //...Object.keys(obj.__proto__),
+      ...Object.keys(obj.set as object),
+    ]; //.filter((key) => key.startsWith("set"));
+
     keys.forEach((key) => {
-      if (hasTimer(obj[key])) {
+      if (hasTimer(obj.set[key])) {
         console.error(
           "timers not allowed inside set methods. move timers outside the state. get-set-react set methods don't support setTimeout or setInterval timers."
         );
       }
       if (
-        typeof obj[key] === "function" &&
-        obj[key].constructor.name === "Function"
+        typeof obj.set[key] === "function" &&
+        obj.set[key].constructor.name === "Function"
       ) {
-        let retv;
-        let orginalMethod = obj[key];
+        let retv: any = undefined;
+        let orginalMethod = obj.set[key];
         let temp = function (...props: any) {
-          retv = orginalMethod.call(obj, ...props);
-          update(obj);
-          return retv;
+          try {
+            retv = orginalMethod.call(obj, ...props);
+            if (!obj.__.onSetInProgress) {
+              update(obj);
+              obj.__.setList.push(temp);
+            }
+          } catch (e: any) {
+            catchError(e, key);
+          } finally {
+            return retv;
+          }
         };
-        obj[key] = temp;
+        obj.set[key] = temp;
       } else if (
-        typeof obj[key] === "function" &&
-        obj[key].constructor.name === "GeneratorFunction"
+        typeof obj.set[key] === "function" &&
+        obj.set[key].constructor.name === "GeneratorFunction"
       ) {
-        console.error("get-set-react does not support generator functions. ");
+        console.error(
+          "get-set-react does not support generator functions at this moment. "
+        );
       } else if (
-        typeof obj[key] === "function" &&
-        obj[key].constructor.name === "AsyncFunction"
+        typeof obj.set[key] === "function" &&
+        obj.set[key].constructor.name === "AsyncFunction"
       ) {
         let retv: any;
-        let orginalMethod = obj[key];
-        let clear = setInterval(() => {
-          update(obj);
-        }, 500);
+        let orginalMethod = obj.set[key];
+
         let temp = async function (...props: any) {
+          let clear = setInterval(() => {
+            !obj.__.onSetInProgress && update(obj);
+          }, 500);
           try {
             retv = await orginalMethod.call(obj, ...props);
           } catch (e) {
-            console.error("error occured in this async function.");
-            return retv;
+            catchError(e, key);
           } finally {
             if (clear) {
-              update(obj);
+              if (!obj.__.onSetInProgress) {
+                update(obj);
+                obj.__.setList.push(temp);
+              }
               clearInterval(clear);
             }
           }
           return retv;
         };
-        obj[key] = temp;
+        obj.set[key] = temp;
       }
     });
-    obj.__ = new ActionStore();
   }
 }
-
-//c.set_increment();
-
-//console.log(counter.async_fetchUsers.constructor.name);
-
-// function stateChanged(obj) {
-//   console.log("state changed for ", obj);
-// }
 
 export let subScribe = function (obj: any, func: any, label: any) {
   let subscription = obj.__.subscriptions.find((d: any) => d.label === label);
@@ -86,15 +114,7 @@ export let subScribe = function (obj: any, func: any, label: any) {
     obj.__.subscriptions.push({ label: label, action: func });
   }
 };
-export let update = function (
-  obj: any,
-  // method?: any,
-  throttle = 50,
-  updateSiblings = false
-) {
-  //method && method.apply(obj, props);
-
-  obj?.onChange && obj.onChange();
+let update = function (obj: any, throttle = 50, updateSiblings = false) {
   obj && stateChanged(obj, throttle, updateSiblings);
 };
 export let updateAll = () => {};
@@ -103,6 +123,29 @@ export let unSubscribe = function (obj: any, label: any) {
     (d: any) => d.label !== label
   );
 };
+
+export const setEffect = (fn: Function, deps: Function[]) => {
+  if (
+    !stateUpdateContext ||
+    !stateUpdateContext.__ ||
+    !stateUpdateContext.__.setList
+  ) {
+    console.error("something went wrong");
+    return;
+  } else {
+    if (
+      deps.length === 0 ||
+      deps.find((dep) =>
+        Object.keys(stateUpdateContext.__.setList).find(
+          (key) => stateUpdateContext.__.setList[key] === dep
+        )
+      )
+    ) {
+      fn && fn();
+    }
+  }
+};
+
 export let stateChanged = function (
   state: any,
   throttle: number,
@@ -110,6 +153,19 @@ export let stateChanged = function (
 ) {
   if (!state.__.clearTimeOut) {
     state.__.clearTimeOut = window.setTimeout(() => {
+      if (state.onSet) {
+        state.__.onSetInProgress = true;
+        stateUpdateContext = state;
+        try {
+          state.onSet();
+        } catch (e) {
+          console.error("error in onSet", e);
+        } finally {
+          state.__.setList = [];
+          state.__.onSetInProgress = false;
+          stateUpdateContext = null;
+        }
+      }
       let subScriptions = [];
 
       if (!updateSiblings) subScriptions = state.__.subscriptions;
@@ -139,7 +195,6 @@ export const getRandom = (length: number) => {
 };
 const subscriptions: any = {};
 function updateComponent(label: string, throttle: number = 50) {
-  console.log(subscriptions);
   if (
     subscriptions[label] &&
     typeof subscriptions[label] == "object" &&
@@ -154,7 +209,7 @@ function updateComponent(label: string, throttle: number = 50) {
   }
 }
 
-export const useGetSet = function (...props: any) {
+export const useGetSet = function (props: any[]) {
   const [refresh, setRefresh] = useState(0);
   const [uniqueCode] = useState(getRandom(8));
   props
